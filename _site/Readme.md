9848be6fa6bbc68af76babe6b8f4017378fc6a73
Tim Whitacre <tim@argyllstudios.com>
2014-04-21 11:34:34 -0400

    Removed jquery and pushstate (not needed), added whiskey list and updated styles

---
+++ _site/Readme.md
@@ -0,0 +1,7 @@
+### About This Site
+
+---
+
+This is my personal site [Tim Whitacre](http://timw.co).
+
+This theme is basically just a re-work of the Jekyll theme [hikari](https://github.com/m3xm/hikari-for-Jekyll) 
--- js/main.js
+++ js/main.js
@@ -1,50 +1,18 @@
-$(document).ready(function(){
+var $menuIcon = document.getElementsByClassName("menu-icon")[0],
+    $offCanva = document.getElementById('off-canvas');
 
-    var $menuIcon = document.getElementsByClassName("menu-icon")[0],
-        $offCanva = document.getElementById('off-canvas');
+$menuIcon.addEventListener('click', function() {
+    toggleClass($menuIcon, 'close');
+    toggleClass($offCanva, 'toggled');
+}, false);
 
-    $menuIcon.addEventListener('click', function() {
-        toggleClass($menuIcon, 'close');
-        toggleClass($offCanva, 'toggled');
-    }, false);
-
-    function toggleClass(element, className) {
-        if (!element || !className) { return; }
-        var classString = element.className, nameIndex = classString.indexOf(className);
-        if (nameIndex == -1) { 
-            classString += ' ' + className;
-        } else {
-            classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
-        }
-        element.className = classString;
+function toggleClass(element, className) {
+    if (!element || !className) { return; }
+    var classString = element.className, nameIndex = classString.indexOf(className);
+    if (nameIndex == -1) { 
+        classString += ' ' + className;
+    } else {
+        classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
     }
-
-    // Push State
-    if (history.pushState) {
-        var selector = "#blogroll",
-        loadSelector = " " + selector + " > *",
-        container = $(selector).on("click", "li[role='pagenavigation'] a", function() {
-            var href = $(this).attr("href");
-            $("html, body").animate({ "scrollTop": 0 }, 400);
-            history.pushState({ "path": href }, null, href);
-            container.load(href + loadSelector);
-            return false;
-        });
-
-        $(window).bind("popstate", function(e) {
-            var state = e.originalEvent.state;
-            if (state) {
-                $("window, body").animate({ "scrollTop": 0 }, 200);
-                    container.load(state.path + loadSelector);
-                }
-            });
-        history.replaceState({ "path": location.href }, "");
-    }
-
-    // Whiskey Checklist
-    $('.checkList li').on('click', function (e) {
-        $(this).toggleClass('drank');
-    });
-
-
-});
+    element.className = classString;
+}
