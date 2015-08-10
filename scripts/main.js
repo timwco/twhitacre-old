f5a63f4ce471a0ebf922839a9585b783765b4dbc
Tim Whitacre <tim@timw.co>
2015-08-10 12:23:42 -0400

    Putting the blog post link back in

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
-// $.ajax({
-//   url: 'http://blog.timw.co/',
-//   type: 'get',
-//   dataType: 'html',
-// }).done(function (data){
-//   $(data).find('.content article').each(function () {
-//     var art = $(this).find('h2.entry-title a');
-//     var link = art.attr('href');
-//     var title = art.text(); 
-//     var date = art.parent().next('.date').text();
-//     $('#blog_articles ul').append('<li><a href="'+link+'">'+title+'</a> -  <span>'+date+'</span></li>');
-//   });
-// });
+$.ajax({
+  url: 'http://blog.timw.co/',
+  type: 'get',
+  dataType: 'html',
+}).done(function (data){
+  $(data).find('.content article').each(function () {
+    var art = $(this).find('h2.entry-title a');
+    var link = art.attr('href');
+    var title = art.text(); 
+    var date = art.parent().next('.date').text();
+    $('#blog_articles ul').append('<li><a href="'+link+'">'+title+'</a> -  <span>'+date+'</span></li>');
+  });
+});
 
